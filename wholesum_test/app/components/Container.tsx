f99d1037b95aeb3963aa25dc0cbb735a4abb46f2
정민규 <jmg2165@naver.com>
2023-11-07 16:24:01 +0900

    design : Navbar hover시에 서브 메뉴 슬라이드로 생성

--- wholesum_test/app/components/Container.tsx
+++ wholesum_test/app/components/Container.tsx
@@ -8,7 +8,7 @@ interface ContainerProps {
 export const Container: React.FC<ContainerProps> = ({ children }) => {
     return (
         <>
-            <div className="mx-auto px-4 w-full">{children}</div>
+            <div className="mx-auto w-full">{children}</div>
         </>
     );
 };
--- wholesum_test/app/components/Navbar.tsx
+++ wholesum_test/app/components/Navbar.tsx
@@ -2,14 +2,25 @@
 
 import { useRouter } from "next/navigation";
 import { border, colors, size } from "../type/constants";
+import { useMemo, useCallback, useState } from "react";
 
 // 네비게이션 컴포넌트
 export const Navbar: React.FC = () => {
     const router = useRouter();
+    const [isHovering, setIsHovering] = useState(false);
+
+    //최적화를 위해 useMemo 사용
+    const dropdownClasses = useMemo(() => {
+        return `absolute w-full overflow-hidden z-10 transition-all duration-500 ease-out ${
+            isHovering ? "max-h-full" : "max-h-0"
+        }`;
+    }, [isHovering]);
+
     return (
         <>
             <div
-                className={`flex items-center gap-5 p-2 ${border.borderBottom} ${colors.borderColor}`}
+                className={`flex items-center gap-5 p-2 ${colors.borderColor}`}
+                onMouseEnter={() => setIsHovering(true)}
             >
                 <div className="justify-self-center ml-10">
                     <p
@@ -21,15 +32,40 @@ export const Navbar: React.FC = () => {
                         POTTERY
                     </p>
                 </div>
-                <div>
-                    <p
-                        className="cursor-pointer"
-                        onClick={() => {
-                            router.push("/productPage");
-                        }}
-                    >
-                        제품
-                    </p>
+                <div className="justify-self-center ml-10">
+                    <p className="cursor-pointer">제품</p>
+                </div>
+            </div>
+            <div
+                className={dropdownClasses}
+                onMouseLeave={() => setIsHovering(false)}
+            >
+                <div
+                    className={`p-2 ${colors.textColor} w-full border-neutral-200 border-2 grid grid-cols-5 gap-4 bg-white`}
+                >
+                    <div>
+                        <p className="mb-3 text-xl">나중에 사용할 공간</p>
+                    </div>
+
+                    <div className="flex flex-col">
+                        <p className="mb-3 text-xl">카테고리</p>
+                        <p className="cursor-pointer inline-block self-start">
+                            셔츠
+                        </p>
+                        <p className="cursor-pointer inline-block self-start">
+                            데님
+                        </p>
+                    </div>
+
+                    <div>
+                        <p className="mb-3 text-xl">나중에 사용할 공간</p>
+                    </div>
+                    <div>
+                        <p className="mb-3 text-xl">나중에 사용할 공간</p>
+                    </div>
+                    <div>
+                        <p className="mb-3 text-xl">나중에 사용할 공간</p>
+                    </div>
                 </div>
             </div>
         </>
